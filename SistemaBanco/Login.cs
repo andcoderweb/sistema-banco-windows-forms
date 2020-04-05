d0734c447d732bf569e5f59fc88c1da2ad5de4b8
andcoderweb <andersoncoderweb@gmail.com>
2020-04-05 09:09:03 -0300

    sisteminha banco cadastro e depósito

---
+++ SistemaBanco/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaBanco
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string nome = textBox1.Text;
+            Session s = new Session(nome);
+            if (textBox1.Text == String.Empty)
+            {
+                MessageBox.Show("Preencha o nome para continuar", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (textBox2.Text.Length!=5) {
+                MessageBox.Show("A conta deve ter 5 Digitos", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (textBox3.Text.Length != 6)
+            {
+                MessageBox.Show("A senha deve ter 6 Digitos", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            s.ShowDialog();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            this.Text = "Bem-vindo-" + DateTime.Now.ToShortDateString();
+
+        }
+    }
+}
---
+++ SistemaBanco/Session.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaBanco
+{
+    public partial class Session : Form
+    {
+        public Session(string nome)
+        {
+            InitializeComponent();
+            label2.Text = nome;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+                double valor1 = Convert.ToDouble(textBox1.Text,CultureInfo.InvariantCulture);
+            if (valor1<=0)
+            
+                MessageBox.Show("Deposite valor maior do que 0");
+
+            
+
+            label5.Text = Convert.ToString(valor1);
+           
+            
+ 
+
+            
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+       
+
+          
+    }
+}
